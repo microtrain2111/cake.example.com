6ac68ed6cd358788569ad84be92ed6c07604fbbb
chad svastisalee <swasgrafx@gmail.com>
2021-12-14 11:55:32 -0600

    Added Cake Users Login

--- config/app.php
+++ config/app.php
@@ -180,12 +180,14 @@ return [
      *   your application that still emit deprecations.
      */
     'Error' => [
-        'errorLevel' => E_ALL,
+        'errorLevel' => E_ALL & ~E_USER_DEPRECATED,
         'exceptionRenderer' => ExceptionRenderer::class,
         'skipLog' => [],
         'log' => true,
         'trace' => true,
-        'ignoredDeprecationPaths' => [],
+        'ignoredDeprecationPaths' => [
+            'vendor/cakephp/cakephp/src/Routing/Route/Route.php',
+        ],
     ],
 
     /*
@@ -225,7 +227,9 @@ return [
      */
     'EmailTransport' => [
         'default' => [
-            'className' => MailTransport::class,
+            // turn on for email through server
+            // 'className' => MailTransport::class,
+            'className' => 'Debug',
             /*
              * The keys host, port, timeout, username, password, client and tls
              * are used in SMTP transports
